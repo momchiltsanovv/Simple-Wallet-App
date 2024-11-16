b80a12dedc8ce027be6ac2d0265b818500e0020a
Momchil Tsanov <momchil395@gmail.com>
2024-11-16 20:49:36 +0200

    all done

---
+++ Main.java
@@ -0,0 +1,11 @@
+import core.Engine;
+
+// nth tier architecture
+public class Main {
+
+    public static void main(String[] args) {
+
+        Engine engine = new Engine();
+        engine.run();
+    }
+}
